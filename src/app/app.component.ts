ded9ac4f0d41b38d6ef1ae2f733e9acd47708be2
Sergei Solovev <sergey.soloviev@inbox.ru>
2021-12-14 17:06:12 +0300

    api service implemented, users module services scaffoled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { ApiService } from './core/services/api.service';
 
 @Component({
   selector: 'qoollo-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [],
+  viewProviders: [],
 })
 export class AppComponent {
-  title = 'qoollo-users-list';
+
+  constructor() {}
+
 }
